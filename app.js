6ec17c80e0ef5c170a951b1a20a92b438781b991
foggybrains <dustin.keyes@gmail.com>
2020-01-22 17:19:15 -0500

    tinker my heart strings

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ const communiteePage = 'http://localhost:3000/'
 const usersUrl = 'http://localhost:3000/users'
 const postsUrl = 'http://localhost:3000/posts'
 const tasksUrl = 'http://localhost:3000/tasks'
+const commentsUrl = 'http://localhost:3000/comments'
 
 let userIndex = {}
 let currentUser = {}
@@ -56,6 +57,16 @@ function resetPageLite() {
     }
 }
 
+// adding new friends (optimistic rendering only)
+friendForm.addEventListener('submit', event => {
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    const friend = formData.get('friend')
+    displayFriend(friend)
+        // add code to postFriend(friend) to back end
+    event.target.reset()
+})
+
 // adding new tasks
 taskForm.addEventListener('submit', event => {
     event.preventDefault()
@@ -65,20 +76,6 @@ taskForm.addEventListener('submit', event => {
     postTask(task)
     event.target.reset()
 })
-// adding new friends (optimistic rendering only)
-friendForm.addEventListener('submit', event => {
-    event.preventDefault()
-    const formData = new FormData(event.target)
-    const friend = formData.get('friend')
-    displayFriend(friend)
-        // postFriend(friend)
-    event.target.reset()
-})
-
-// function postFriend(friend) {
-
-// }
-
 function displayTask(task) {
     const div = document.createElement('div')
     const p = document.createElement('p')
@@ -90,7 +87,6 @@ function displayTask(task) {
     div.appendChild(p)
     taskList.appendChild(div)
 }
-
 function postTask(task) {
     const taskObject = {
         content: task,
@@ -104,23 +100,21 @@ function postTask(task) {
         }
     })
 }
+
 // adds event delegation for trash class WIDGET
 widgets.addEventListener('click', event => {
         if (event.target.className == 'trash') {
             event.target.parentNode.remove();
         }
     })
-    // add new post front and back end
+
+// adding new posts
 postForm.addEventListener('submit', event => {
     event.preventDefault()
     const formData = new FormData(event.target)
     const postContent = formData.get('postInput')
     let postAnon = formData.get('anon')
-    if (postAnon == null) {
-        postAnon = false
-    } else {
-        postAnon = true
-    }
+    postAnon == null ? postAnon = false : postAnon = true
     const postObject = {
         user_id: currentUser.data.id,
         anon: postAnon,
@@ -128,22 +122,63 @@ postForm.addEventListener('submit', event => {
         splash: 0,
         content: postContent
     }
-    console.log(postObject)
+    // *back end = crash, front end = bash*
     event.target.reset()
     displayPost(postObject)
     postThePost(postObject)
 })
+// optimistically rendering post
+function displayPost(post) {
+    console.log('display post function firing')
+    const postCard = document.createElement('div')
+    postCard.className = 'card'
 
+    const postButtonDiv = document.createElement('div')
+    postButtonDiv.className = 'post-button-div'
+
+    // const splashForm = document.createElement('form')
+    // splashForm.innerHTML = `
+    // <span name='count'>0</span>
+    // <input type='checkbox' name='splash' for='count'>
+    // <label for='splash'>Splash</label>`
+    // postButtonDiv.appendChild(splashForm)
+
+    const content = document.createElement('p')
+    styleContent(content, post)
+    // splashes
+    const splashes = document.createElement('span')
+    styleSplashes(splashes)
+    const splashButton = document.createElement('button')
+    styleSplashButton(splashButton, post)
+    // bashes
+    const bashes = document.createElement('span')
+    styleBashes(bashes)
+    const bashButton = document.createElement('button')
+    styleBashButton(bashButton, post)
+
+    // const comment = document.createElement('p')
+    // styleComment(comment, post)
+
+    // const input = document.createElement('input')
+    // styleCommentInput(input)
+
+    // const commentButton = document.createElement('button')
+    // styleCommentButton(commentButton)
+
+    splashes.appendChild(splashButton)
+    bashes.appendChild(bashButton)
+    postButtonDiv.append(splashes, bashes)
+    postCard.append(content, postButtonDiv)
+    postList.appendChild(postCard)
+}
 function postThePost(post) {
     fetch(postsUrl, {
         method: 'POST',
         body: JSON.stringify(post),
-        headers: {
-            'Content-Type': 'application/json'
-        }
+        headers: { 'Content-Type': 'application/json' }
     })
 }
-
+// theme control center
 const themes = {
     manatee: {
         url: "url('https://api.time.com/wp-content/uploads/2017/03/manatee-endangered-species.jpeg')",
@@ -191,83 +226,9 @@ const themes = {
         poolColor: '#FEFCFD'
     }
 }
-
 themeSelect.addEventListener('change', event => {
     changeTheme(event.target.value)
 })
-
-loginButton.addEventListener('click', event => {
-    findProfile(profileInput.value)
-    profileInput.value = ''
-})
-
-function findProfile(user) {
-    fetch(usersUrl)
-        .then(response => response.json())
-        .then(response => userIndex = response)
-        .then(array => array.filter(profile => profile.name == user))
-        .then(fetchProfileData)
-}
-
-function fetchProfileData(profile) {
-    console.log(profile)
-    const id = profile[0].id
-    const profileUrl = `http://localhost:3000/users/${id}`
-    fetch(profileUrl)
-        .then(resp => resp.json())
-        .then(displayProfileData)
-}
-// profile is set
-function displayProfileData(profile) {
-    console.log(profile)
-    currentUser = profile
-    const attributes = profile.data.attributes
-    resetPage()
-    setPageHeading(attributes.name)
-    showProfilePicture(attributes.picture)
-    showUserPosts(attributes.posts)
-    showUserTasks(attributes.tasks)
-    changeTheme(attributes.theme)
-    getFriends(profile.data.id)
-}
-
-function setPageHeading(name) {
-    pageHeading.innerText = `${name.toUpperCase()}'s Page`
-}
-
-function showProfilePicture(url) {
-    const urlString = `url('${url}')`
-    profilePicDiv.style.backgroundImage = urlString
-}
-
-function showUserTasks(tasks) {
-    tasks.map(task => displayTask(task.content))
-}
-
-function showUserPosts(posts) {
-    posts.map(post => displayPost(post))
-}
-
-function getFriends(id) {
-    const friendsUrl = `http://localhost:3000/users/${id}/friends`
-    fetch(friendsUrl)
-        .then(resp => resp.json())
-        .then(friendArray => friendArray.map(
-            friend => displayFriend(friend.name)))
-}
-
-function displayFriend(name) {
-    const div = document.createElement('div')
-    const friend = document.createElement('p')
-    const trash = document.createElement('button')
-    trash.className = 'trash'
-    trash.innerText = 'Trash'
-    friend.innerText = name
-    friend.appendChild(trash)
-    div.appendChild(friend)
-    friendList.appendChild(div)
-}
-
 function changeTheme(animal) {
     themeImage.style.backgroundImage = themes[animal]['url']
     themeImage.style.backgroundSize = themes[animal]['backgroundSize']
@@ -286,6 +247,70 @@ function changeTheme(animal) {
     postList.style.background = themes[animal]['poolColor']
 }
 
+loginButton.addEventListener('click', event => {
+    findProfile(profileInput.value)
+    profileInput.value = ''
+})
+function findProfile(user) {
+    fetch(usersUrl)
+        .then(response => response.json())
+        .then(response => userIndex = response)
+        .then(array => array.filter(profile => profile.name == user))
+        .then(fetchProfileData)
+}
+function fetchProfileData(profile) {
+    const id = profile[0].id
+    const profileUrl = `http://localhost:3000/users/${id}`
+    fetch(profileUrl)
+        .then(resp => resp.json())
+        .then(displayProfileData)
+}
+// profile is set
+function displayProfileData(profile) {
+    currentUser = profile
+    const attributes = profile.data.attributes
+    resetPage()
+    setPageHeading(attributes.name)
+    showProfilePicture(attributes.picture)
+    showUserPosts(attributes.posts)
+    showUserTasks(attributes.tasks)
+    changeTheme(attributes.theme)
+    getFriends(profile.data.id)
+}
+function setPageHeading(name) {
+    pageHeading.innerText = `${name.toUpperCase()}'s Page`
+}
+function showProfilePicture(url) {
+    const urlString = `url('${url}')`
+    profilePicDiv.style.backgroundImage = urlString
+}
+function showUserPosts(posts) {
+    posts.map(post => displayPost(post))
+}
+function showUserTasks(tasks) {
+    tasks.map(task => displayTask(task.content))
+}
+function getFriends(id) {
+    const friendsUrl = `http://localhost:3000/users/${id}/friends`
+    fetch(friendsUrl)
+        .then(resp => resp.json())
+        .then(friendArray => friendArray.map(
+            friend => displayFriend(friend.name)
+        ))
+}
+
+function displayFriend(name) {
+    const div = document.createElement('div')
+    const friend = document.createElement('p')
+    const trash = document.createElement('button')
+    trash.className = 'trash'
+    trash.innerText = 'Trash'
+    friend.innerText = name
+    friend.appendChild(trash)
+    div.appendChild(friend)
+    friendList.appendChild(div)
+}
+
 appName.addEventListener('click', event => {
     if (currentUser == {}) {
         resetPage()
@@ -294,10 +319,16 @@ appName.addEventListener('click', event => {
         loadAllPosts()
     }
 })
+function loadAllPosts() {
+    fetch(postsUrl)
+        .then(res => res.json())
+        .then(array => array.map(
+            post => displayPost(post)
+        ))
+}
 
-// splash/bash incrementer
+// splash/bash incrementer (optimistically rendered)
 posts.addEventListener('click', event => {
-    console.log(event.target)
     if (event.target.className == 'post-button') {
         event.target.innerText++
     }
@@ -308,63 +339,9 @@ userLink.addEventListener('click', event => {
     fetchProfileData(array)
 })
 
-function loadAllPosts() {
-    fetch(postsUrl)
-        .then(res => res.json())
-        .then(array => array.map(
-            post => displayPost(post)))
-}
-
-// only display post front end
-function displayPost(post) {
-    const postCard = document.createElement('div')
-    postCard.className = 'card'
-
-    const postButtonDiv = document.createElement('div')
-    postButtonDiv.className = 'post-button-div'
-
-    // const splashForm = document.createElement('form')
-    // splashForm.innerHTML = `
-    // <span name='count'>0</span>
-    // <input type='checkbox' name='splash' for='count'>
-    // <label for='splash'>Splash</label>`
-    // postButtonDiv.appendChild(splashForm)
-    const content = document.createElement('p')
-    styleContent(content, post)
-
-    const splashes = document.createElement('span')
-    styleSplashes(splashes)
-
-    const bashes = document.createElement('span')
-    styleBashes(bashes)
-
-    // const comment = document.createElement('p')
-    // styleComment(comment, post)
-
-    // const input = document.createElement('input')
-    // styleCommentInput(input)
-
-    // const commentButton = document.createElement('button')
-    // styleCommentButton(commentButton)
-
-    const splashButton = document.createElement('button')
-    styleSplashButton(splashButton, post)
-
-    const bashButton = document.createElement('button')
-    styleBashButton(bashButton, post)
-
-    splashes.appendChild(splashButton)
-    bashes.appendChild(bashButton)
-    postButtonDiv.append(splashes, bashes)
-    postCard.append(content, postButtonDiv)
-    postList.appendChild(postCard)
-}
-
 function styleContent(content, post) {
-    // console.log(post)
     if (post.anon == true) {
-        console.log('post.anon is true!')
-        content.innerText = `Anonymous Hippo: ${post.content}`
+        content.innerText = `Anony-Moose: ${post.content}`
     } else {
         userIndex.forEach(user => {
             if (user.id == post.user_id) {
